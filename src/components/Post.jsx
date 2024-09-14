96c0bd7b7ab5e4729e401082c5efb26afbd6e441
siddhantteotia30 <siddhantteotia30@gmail.com>
2024-09-14 18:25:43 +0000

    added react icons

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,19 @@
+import { MdDelete } from "react-icons/md";
 const Post = ({ post }) => {
     return (
-        <div className="card post-card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
+        <div className="card post-card" style={{ width: "30rem" }}>
             <div className="card-body">
-                <h5 className="card-title">{post.title}</h5>
+                <h5 className="card-title">{post.title}
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                <MdDelete />
+                </span>
+                </h5>
                 <p className="card-text">{post.body}</p>
                 {post.tags.map((tag) => (<span className="badge bg-primary hashtag">{tag}</span>
                 ))}
+                <div className="alert alert-success reactions" role="alrt">
+                    
+                </div>
             </div>
         </div>
     )
